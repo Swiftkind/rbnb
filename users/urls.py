c46b813feaf36e6e854a2bd9f7544890de634acf
Christian Mae Mendez <Christianmae041997@gmail.com>
2018-10-11 20:06:18 +0800

    login feature

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from users.views import (
     IndexView, 
     SignupView,
+    LoginView,
 )
 
 urlpatterns = [
     path('index/', IndexView.as_view(), name='index'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from django.views.generic import View, TemplateView
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, LoginForm
 
 
 class IndexView(TemplateView):
@@ -23,3 +24,17 @@ class SignupView(TemplateView):
         return render(self.request, self.template_name, {'form': form})
 
 
+class LoginView(TemplateView):
+    template_name = 'users/login.html'
+
+    def get(self, *args, **kwargs):
+        form = LoginForm()
+        return render(self.request, self.template_name, {'form': form})
+
+    def post(self, *args, **kwargs):
+        form = LoginForm(self.request.POST)
+        if form.is_valid():
+            user = form.user_cache
+            login(self.request, user)
+            return HttpResponseRedirect(reverse('index'))
+        return render(self.request, self.template_name, {'from': form})
